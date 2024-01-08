76cf6ca1b8a1e59a6fa96a1b952b76e61edcd7aa
fazaio <faza6028@gmail.com>
2024-01-09 00:54:56 +0700

    hello babe

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="container mx-auto py-5 flex justify-between items-center">
+    <div class="text-xl">Asgindo Herbal</div>
+    <div class="flex">
+      <ElButton link>HOME</ElButton>
+      <ElButton link>ABOUT</ElButton>
+      <ElButton link>FAQ</ElButton>
+      <ElButton link>CONTACT</ElButton>
+    </div>
+  </header>
+  <section>
+      <NuxtPage />
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- section 1 -->
+    <section class="bg-gray-50 flex items-center" style="height: 500px;">
+        <div class="container mx-auto text-gray-800">
+            <div class="grid grid-cols-2">
+                <div class="flex items-center">
+                    <div>
+                        <p class="text-3xl mb-2">Herbal. Natural. Organic.</p>
+                        <p class="w-2/3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nulla aliquid quia, eaque totam
+                            magnam? Rem ex dolor illo officia tempore architecto? Ea totam iure velit placeat fuga saepe
+                            soluta. . . .</p>
+                        <ElButton type="warning" class="mt-2">Selengkapnya</ElButton>
+                    </div>
+                </div>
+                <div class="flex justify-center">
+                    <img src="@/assets/img/product-1.png" class="h-72" alt="" srcset="">
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
